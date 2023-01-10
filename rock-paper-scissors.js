6471bc53c79d8edc3c667150604ec99799c4a727
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-10 15:59:58 +0100

    Check if player beats computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,15 +6,15 @@ let playerSelection = "";
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNumber === 2){
-        return "Paper"
+        return "paper"
     }
-    else return "Scissors";
+    else return "scissors";
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 computerSelection = getComputerChoice();
 
 
@@ -22,7 +22,58 @@ computerSelection = getComputerChoice();
     // prompt user
     // remember case sensitivity
     // catch inappropriate input
+
+playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+console.log(playerSelection);
+
+function checkChoiceValidity(option) {
+    if (option != "rock" && option != "paper" && option != "scissors") {
+        return "Please enter a valid option";
+    }
+    return "Great!";
+}
+
+console.log(checkChoiceValidity(playerSelection));
+
 // check if user input beats computer choice
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie"
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return declareWinner(playerSelection, computerSelection);
+    }
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return declareLoser(playerSelection, computerSelection);
+    }
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return declareWinner(playerSelection, computerSelection);
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return declareLoser(playerSelection, computerSelection);
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return declareWinner(playerSelection, computerSelection);
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return declareLoser(playerSelection, computerSelection);
+    }
+    return "Oops, something went wrong";
+}
+
+function declareWinner(winner, loser) {
+    winner = winner.slice(0,1).toUpperCase() + winner.slice(1);
+    return "You win! " + winner + " beats " + loser;
+}
+
+function declareLoser(loser, winner) {
+    winner = winner.slice(0,1).toUpperCase() + winner.slice(1);
+    return "You lose! " + winner + " beats " + loser;
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
 // display result
 // keep user score
 // keep computer score
